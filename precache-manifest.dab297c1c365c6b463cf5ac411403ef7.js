37aeb09df22cbc968eec8cca223e95c0d5f75526
luz202010 <73671396+luz202010@users.noreply.github.com>
2022-05-28 08:38:52 +0800

    Updates

---
+++ precache-manifest.dab297c1c365c6b463cf5ac411403ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4f4cda924061026f4701da110dea64",
+    "url": "/Ecommerce_website/index.html"
+  },
+  {
+    "revision": "bcc29c43d030afbcdd42",
+    "url": "/Ecommerce_website/static/css/main.354cf012.chunk.css"
+  },
+  {
+    "revision": "ad214d01140c1dedd30f",
+    "url": "/Ecommerce_website/static/js/2.73adbce5.chunk.js"
+  },
+  {
+    "revision": "60b3816fc1b5b090afdc8f95428879ee",
+    "url": "/Ecommerce_website/static/js/2.73adbce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc29c43d030afbcdd42",
+    "url": "/Ecommerce_website/static/js/main.6b5d827a.chunk.js"
+  },
+  {
+    "revision": "04a04256ea863950588c",
+    "url": "/Ecommerce_website/static/js/runtime-main.0c208184.js"
+  }
+]);
